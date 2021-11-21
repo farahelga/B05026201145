8bf4104ee55edb3dd1ed8b5ea241777ede02f8a6
farahelga <farah.helga@gmail.com>
2021-11-21 20:34:18 +0700

    minggu12
    Mengubah dan menghapus data dengan laravel

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::get('/pegawai','PegawaiController@index');
 
 Route::get('/pegawai/tambah','PegawaiController@tambah');
 Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
